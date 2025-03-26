3420228b707263d9df362b4c0fc25002b04acc87
Jordan Cazares <l.caej040411@itses.edu.mx>
2025-03-25 20:42:59 -0600

    Actualizacion de nuevos componentes

---
+++ AlphaSystemFront/src/app/principal/components/quienes/quienes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quienes',
+  imports: [],
+  templateUrl: './quienes.component.html',
+  styleUrl: './quienes.component.scss'
+})
+export class QuienesComponent {
+
+}
